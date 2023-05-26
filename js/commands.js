584e66819b452f89a727c729d52c96d05267d073
tiangrumerec <61188262+TeekiCheekiBreeki@users.noreply.github.com>
2023-05-26 17:25:14 +0200

    Finished portfolio website

---
+++ js/commands.js
@@ -0,0 +1,89 @@
+var youtube = "https://www.youtube.com/channel/UCMYiYDYUNzC7WuXTDBejnAQ";
+var password = "r3tr0";
+var linkedin = "https://www.linkedin.com/in/tian-grumerec-144624278/";
+var instagram = "https://www.instagram.com/ti_an11/";
+var github = "https://github.com/TeekiCheekiBreeki/";
+var email = 'mailto:tgrumerec@gmail.com';
+
+whois = [
+
+        "<br>",
+        "Hey, I'm Tian!👋",
+        "I'm a computer science student from Slovenia. Currently in my third year of study.",
+        "I haven't worked on any bigger projects so far. I'm interested in web design, game and android development",
+        "Stuff I (kind of) know:",
+        "- Java",
+        "- C",
+        "- Kotlin",
+        "- Python",
+        "- HTML",
+        "- CSS",
+        "- JavaScript",
+        "- Docker",
+        "- SQL",
+        "I'm also interested in learning new languages and technologies.",
+        "I'm currently learning Python, how to use Unity ",
+        "Blender.",
+        "In future I want to learn:",
+        "- Flutter",
+          "- React",
+        "<br>"
+      
+
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/tian' + "</a>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/tian-grumerec' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/ti_an11' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/TeekiCheekiBreeki' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who am I?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+    '<span class="index">ForrestKnight (FK) Not A Corporation. All knights reserved.</span>',
+    '___________.__                  ________                                                 ',
+    '\\__    ___/|__|____    ____    /  _____/______ __ __  _____   ___________   ____   ____  ',
+    '  |    |   |  \\__  \\  /    \\  /   \\  __\\_  __ \\  |  \\/     \\_/ __ \\_  __ \\/ __ \\_/ ___\\ ',
+    '  |    |   |  |/ __ \\|   |  \\ \\    \\_\\  \\  | \\/  |  /  Y Y  \\  ___/|  | \\/\\  ___/\\  \\___ ',
+    '  |____|   |__(____  /___|  /  \\______  /__|  |____/|__|_|  /\\___  >__|    \\___  >\\___  >',
+    '                   \\/     \\/          \\/                  \\/     \\/            \\/     \\/ ',
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  ]
+  
